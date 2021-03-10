072d24d545a93ed3abd2c8001d1b8d3631e590fa
Ben Reid <reidben24@gmail.com>
2021-03-09 23:40:28 -0500

    Update main to use engine and engine::State instead of manual window and loop ownership

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,70 @@
+#include <BLIB/Engine.hpp>
 #include <BLIB/Logging.hpp>
 #include <BLIB/Media/Shapes.hpp>
+#include <Properties.hpp>
 #include <SFML/Graphics.hpp>
 
-int main() {
-    sf::RenderWindow window(
-        sf::VideoMode(800, 600, 32), "BLIB Project", sf::Style::Close | sf::Style::Titlebar);
-    BL_LOG_INFO << "Window opened";
+class DemoEngineState : public bl::engine::State {
+public:
+    static constexpr float DegPerSec = 60.f;
 
-    bl::shapes::Triangle triangle({0, 0}, {120, 0}, {60, 120});
-    triangle.setPosition(400, 300);
-    triangle.setFillColor(sf::Color::Red);
+    static bl::engine::State::Ptr create() { return Ptr(new DemoEngineState()); }
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-                BL_LOG_INFO << "Window closed";
-                break;
-            }
-        }
+    virtual const char* name() const { return "DemoEngineState"; }
 
-        triangle.rotate(1.5f);
-
-        window.clear(sf::Color::Cyan);
-        window.draw(triangle);
-        window.display();
-        sf::sleep(sf::milliseconds(30));
+    virtual void makeActive(bl::engine::Engine& engine) {
+        triangle.setPosition(engine.settings().videoMode().width / 2,
+                             engine.settings().videoMode().height / 2);
+        BL_LOG_INFO << "DemoEngineState activated";
     }
 
-    BL_LOG_INFO << "Exiting";
+    virtual void update(bl::engine::Engine& engine, float dt) override {
+        triangle.rotate(dt * DegPerSec);
+    }
+
+    virtual void render(bl::engine::Engine& engine, float lag) override {
+        // Account for lag
+        const float og = triangle.getRotation();
+        triangle.rotate(lag * DegPerSec);
+
+        engine.window().clear(sf::Color::Cyan);
+        engine.window().draw(triangle);
+        engine.window().display();
+
+        triangle.setRotation(og);
+    }
+
+private:
+    bl::shapes::Triangle triangle;
+
+    DemoEngineState()
+    : triangle({0, 0}, {120, 0}, {60, 120}) {
+        triangle.setFillColor(sf::Color::Red);
+    }
+};
+
+int main() {
+    BL_LOG_INFO << "Loading application properties";
+    if (!Properties::load()) {
+        BL_LOG_ERROR << "Failed to load application properties";
+        return 1;
+    }
+
+    BL_LOG_INFO << "Creating engine instance";
+    const bl::engine::Settings engineSettings =
+        bl::engine::Settings()
+            .withVideoMode(sf::VideoMode(800, 600, 32))
+            .withWindowStyle(sf::Style::Close | sf::Style::Titlebar)
+            .withWindowTitle("BLIB Project");
+    bl::engine::Engine engine(engineSettings);
+    BL_LOG_INFO << "Created engine";
+
+    BL_LOG_INFO << "Running engine main loop";
+    if (!engine.run(DemoEngineState::create())) {
+        BL_LOG_ERROR << "Engine exited with error";
+        return 1;
+    }
+
+    BL_LOG_INFO << "Exiting normally";
     return 0;
 }
