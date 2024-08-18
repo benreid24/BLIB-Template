1f1bc9b95f88041394184ffe253e95df1b5d7028
Ben Reid <reidben24@gmail.com>
2024-08-18 19:06:08 -0400

    Update for latest BLIB

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,45 @@
+#include <BLIB/Cameras/2D/Camera2D.hpp>
 #include <BLIB/Engine.hpp>
+#include <BLIB/Graphics.hpp>
 #include <BLIB/Logging.hpp>
-#include <BLIB/Media/Shapes.hpp>
 #include <Properties.hpp>
-#include <SFML/Graphics.hpp>
 
 class DemoEngineState : public bl::engine::State {
 public:
-    static constexpr float DegPerSec = 60.f;
+    static constexpr float DegPerSec = 90.f;
 
     static bl::engine::State::Ptr create() { return Ptr(new DemoEngineState()); }
 
     virtual const char* name() const { return "DemoEngineState"; }
 
-    virtual void makeActive(bl::engine::Engine& engine) {
-        triangle.setPosition(engine.settings().videoMode().width / 2,
-                             engine.settings().videoMode().height / 2);
+    virtual void activate(bl::engine::Engine& engine) {
+        auto scene = engine.renderer().getObserver().pushScene<bl::rc::scene::Scene2D>();
+        engine.renderer().getObserver().setCamera<bl::cam::Camera2D>(glm::vec2(400, 300),
+                                                                     glm::vec2(800, 600));
+        engine.renderer().getObserver().setClearColor(bl::sfcol(sf::Color::Cyan));
+
+        triangle.create(engine, {0.f, 173.f}, {200.f, 173.f}, {100.f, 0.f});
+        triangle.setColorGradient({1.f, 0.f, 0.f, 1.f}, {0.f, 1.f, 0.f, 1.f}, {0.f, 0.f, 1.f, 1.f});
+        triangle.getTransform().setPosition(400.f, 300.f);
+        triangle.getTransform().setOrigin({100.f, 123.f});
+        triangle.addToScene(scene, bl::rc::UpdateSpeed::Dynamic);
+
         BL_LOG_INFO << "DemoEngineState activated";
     }
 
-    virtual void update(bl::engine::Engine& engine, float dt) override {
-        triangle.rotate(dt * DegPerSec);
+    virtual void deactivate(bl::engine::Engine& engine) override {
+        engine.renderer().getObserver().popScene();
     }
 
-    virtual void render(bl::engine::Engine& engine, float lag) override {
-        // Account for lag
-        const float og = triangle.getRotation();
-        triangle.rotate(lag * DegPerSec);
-
-        engine.window().clear(sf::Color::Cyan);
-        engine.window().draw(triangle);
-        engine.window().display();
-
-        triangle.setRotation(og);
+    virtual void update(bl::engine::Engine&, float dt, float) override {
+        triangle.getTransform().rotate(dt * DegPerSec);
     }
 
 private:
-    bl::shapes::Triangle triangle;
+    bl::gfx::Triangle triangle;
 
     DemoEngineState()
-    : triangle({0, 0}, {120, 0}, {60, 120}) {
-        triangle.setFillColor(sf::Color::Red);
-    }
+    : State(bl::engine::StateMask::Running) {}
 };
 
 int main() {
@@ -53,9 +52,14 @@ int main() {
     BL_LOG_INFO << "Creating engine instance";
     const bl::engine::Settings engineSettings =
         bl::engine::Settings()
-            .withVideoMode(sf::VideoMode(800, 600, 32))
-            .withWindowStyle(sf::Style::Close | sf::Style::Titlebar)
-            .withWindowTitle("BLIB Project");
+            .withWindowParameters(
+                bl::engine::Settings::WindowParameters()
+                    .withVideoMode(sf::VideoMode(800, 600, 32))
+                    .withStyle(sf::Style::Close | sf::Style::Titlebar | sf::Style::Resize)
+                    .withTitle("BLIB Project")
+                    .withLetterBoxOnResize(true)
+                    .fromConfig())
+            .fromConfig();
     bl::engine::Engine engine(engineSettings);
     BL_LOG_INFO << "Created engine";
 
